ecb4b3c0a427eb1a71a07ccb0b4551272fda1fb5
Piotr Poterek <peterpoterek0@gmail.com>
2023-11-29 20:49:51 +0100

    one image at the time render

--- src/cat-api.js
+++ src/cat-api.js
@@ -4,19 +4,43 @@ axios.defaults.headers.common['x-api-key'] =
   'live_TaIGK3rsMEKklkFcwlsXQnzMHzIcWi9oBOFV02xixfWvhpAsIvH0VzMASm6lgRn4';
 
 const breedSelect = document.querySelector('.breed-select');
+const catInfo = document.querySelector('.cat-info');
 
 const fetchCatByBreed = breedId => {
   axios
     .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
-    .then(res => console.log(res.data))
+    .then(res => {
+      console.log(res.data[0].url);
+      let catTitle, catDescription, catTemperament, catImage;
+
+      if (catInfo.children.length === 0) {
+        catTitle = document.createElement('h1');
+        catTitle.setAttribute('class', 'cat-info-title');
+
+        catDescription = document.createElement('p');
+        catDescription.setAttribute('class', 'cat-info-desc');
+
+        catTemperament = document.createElement('p');
+        catTemperament.setAttribute('class', 'cat-info-temperament');
+
+        catImage = document.createElement('img');
+        catImage.setAttribute('class', 'cat-info-img');
+
+        catInfo.append(catTitle, catDescription, catTemperament, catImage);
+      } else {
+        catTitle = document.querySelector('.cat-info-title');
+        catDescription = document.querySelector('.cat-info-desc');
+        catTemperament = document.querySelector('.cat-info-temperament');
+        catImage = document.querySelector('.cat-info-img');
+      }
+
+      catTitle.textContent = res.data[0].breeds[0].name;
+      catDescription.textContent = res.data[0].breeds[0].description;
+      catTemperament.innerHTML = `<span>Temperament:</span> ${res.data[0].breeds[0].temperament}`;
+      catImage.setAttribute('src', res.data[0].url);
+    })
     .catch(err => console.log(err));
 };
-
-const handleBreedSelect = e => {
-  const selectedBreed = e.target.value;
-  fetchCatByBreed(selectedBreed);
-};
-breedSelect.addEventListener('change', handleBreedSelect);
 
 const fetchBreeds = () => {
   axios
@@ -34,4 +58,4 @@ const fetchBreeds = () => {
     .catch(err => console.log(err));
 };
 
-export { fetchBreeds };
+export { fetchBreeds, fetchCatByBreed };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-import { fetchBreeds } from './cat-api.js';
+import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
+
+const breedSelect = document.querySelector('.breed-select');
+const handleBreedSelect = e => {
+  const selectedBreed = e.target.value;
+  fetchCatByBreed(selectedBreed);
+};
+
+breedSelect.addEventListener('change', handleBreedSelect);
 
 fetchBreeds();
