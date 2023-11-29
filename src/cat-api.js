fcc8ea6a7571f0346a63f4a535690f34b8c3313d
Piotr Poterek <peterpoterek0@gmail.com>
2023-11-29 18:27:13 +0100

    fetchCatByBreed

--- src/cat-api.js
+++ src/cat-api.js
@@ -4,12 +4,25 @@ axios.defaults.headers.common['x-api-key'] =
   'live_TaIGK3rsMEKklkFcwlsXQnzMHzIcWi9oBOFV02xixfWvhpAsIvH0VzMASm6lgRn4';
 
 const breedSelect = document.querySelector('.breed-select');
+
+const fetchCatByBreed = breedId => {
+  axios
+    .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
+    .then(res => console.log(res.data))
+    .catch(err => console.log(err));
+};
+
+const handleBreedSelect = e => {
+  const selectedBreed = e.target.value;
+  fetchCatByBreed(selectedBreed);
+};
+breedSelect.addEventListener('change', handleBreedSelect);
+
 const fetchBreeds = () => {
   axios
     .get('https://api.thecatapi.com/v1/breeds')
     .then(res => {
       const breedData = res.data;
-      console.log(breedData);
 
       breedData.forEach(breed => {
         const option = document.createElement('option');
